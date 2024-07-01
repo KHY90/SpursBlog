56a6556f7b1ca61505dc716d31750f74f3614d01
KHY90 <power4206@gmail.com>
2024-07-02 00:50:56 +0900

    feat:post-page

--- spring_blog/src/main/java/com/ohgiraffers/blog/hwayeon/controller/HwayeonController.java
+++ spring_blog/src/main/java/com/ohgiraffers/blog/hwayeon/controller/HwayeonController.java
@@ -4,6 +4,7 @@ import com.ohgiraffers.blog.hwayeon.service.HwayeonService;
 import com.ohgiraffers.blog.jaesuk.model.dto.BlogDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,36 +15,47 @@ import org.springframework.web.servlet.ModelAndView;
 public class HwayeonController {
 
     private final HwayeonService hwayeonService;
+    private BlogDTO currentBlog;
 
     @Autowired
     public HwayeonController(HwayeonService hwayeonService) {
         this.hwayeonService = hwayeonService;
     }
 
-    @GetMapping
-    public String indexJaesuck(){
+    @GetMapping("/mainpage")
+    public String mainPage() {
         return "hwayeon/mainpage";
     }
 
-    @PostMapping
-    public ModelAndView postBlog(BlogDTO blogDTO, ModelAndView mv){
-
-        if(blogDTO.getBlogTitle() == null || blogDTO.getBlogTitle().equals("")){
-            mv.setViewName("redirect:hwayeon");
+    @GetMapping("/postpage")
+    public String postPage(Model model) {
+        if (currentBlog != null) {
+            model.addAttribute("blogTitle", currentBlog.getBlogTitle());
+            model.addAttribute("blogContent", currentBlog.getBlogContent());
         }
-        if(blogDTO.getBlogContent() == null || blogDTO.getBlogContent().equals("")){
-            mv.setViewName("redirect:hwayeon");
+        return "hwayeon/postpage";
+    }
+
+    @PostMapping
+    public ModelAndView postBlog(BlogDTO blogDTO, ModelAndView mv) {
+        if (blogDTO.getBlogTitle() == null || blogDTO.getBlogTitle().equals("")) {
+            mv.setViewName("redirect:/hwayeon/mainpage");
+            return mv;
+        }
+        if (blogDTO.getBlogContent() == null || blogDTO.getBlogContent().equals("")) {
+            mv.setViewName("redirect:/hwayeon/mainpage");
+            return mv;
         }
 
         int result = hwayeonService.post(blogDTO);
 
-        if(result <= 0){
+        if (result <= 0) {
             mv.setViewName("error/page");
-        }else{
-            mv.setViewName("hwayeon/mainpage");
+        } else {
+            currentBlog = blogDTO;
+            mv.setViewName("redirect:/hwayeon/postpage");
         }
 
         return mv;
     }
-
 }
