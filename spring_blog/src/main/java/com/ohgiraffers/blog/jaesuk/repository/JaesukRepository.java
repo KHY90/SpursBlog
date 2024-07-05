b147b2b93dd3b6e907fbe08e78b60e98475e5c00
Quokka <wotjr1142@gmail.com>
2024-07-05 10:14:40 +0900

    feat kjs blog

--- spring_blog/src/main/java/com/ohgiraffers/blog/jaesuk/repository/JaesukRepository.java
+++ spring_blog/src/main/java/com/ohgiraffers/blog/jaesuk/repository/JaesukRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JeasuckRepository extends JpaRepository<JaesukBlog, Integer> {
+public interface JaesukRepository extends JpaRepository<JaesukBlog, Integer> {
 
 
 }
